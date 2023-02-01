bad21b9010e7f9ee4d72c8b3841f0f7c57c8206f
Jake Steck <jakestecklownj@gmail.com>
2023-01-31 21:21:08 -0500

    Added Modal identifiers

--- components/ImpEvents.js
+++ components/ImpEvents.js
@@ -7,25 +7,56 @@ import { useState } from 'react';
 
 export default function ImpEvents() {
     const slides = ['https://patch.com/img/cdn20/users/22844250/20200421/014148/styles/patch_image/public/livingston-highschool-schoolpr___21133742706.jpg', 'https://patch.com/img/cdn/users/22844250/2015/05/raw/20150555523bf088bd6.jpg', 'https://patch.com/img/cdn/users/22821264/2014/08/raw/5400e8ea02f3c.png']
-    const [modalVisible, setModalVisible] = useState(false);
+
+    const [modalVisible1, setModalVisible1] = useState(false);
+    const [modalVisible2, setModalVisible2] = useState(false);
+    const [modalVisible3, setModalVisible3] = useState(false);
+
 
 
     return (
         <>
+
             <Modal
                 animationType="slide"
                 presentationStyle='pageSheet'
-                visible={modalVisible}
-                style={{ backgroundColor: 'red' }}
+                visible={modalVisible1}
                 onRequestClose={() => {
-                    setModalVisible(!modalVisible);
+                    setModalVisible1(!modalVisible1);
                 }}>
                 <View style={{ position: 'absolute', backgroundColor: 'grey', top: 10, alignContent: 'center', height: 5, width: '10%', alignSelf: 'center', borderRadius: 10 }}>
                 </View>
+                <Text style={{ fontSize: 80, alignSelf: 'center' }}>1</Text>
             </Modal>
+
+            <Modal
+                animationType="slide"
+                presentationStyle='pageSheet'
+                visible={modalVisible2}
+                onRequestClose={() => {
+                    setModalVisible2(!modalVisible2);
+                }}>
+                <View style={{ position: 'absolute', backgroundColor: 'grey', top: 10, alignContent: 'center', height: 5, width: '10%', alignSelf: 'center', borderRadius: 10 }}>
+                </View>
+                <Text style={{ fontSize: 80, alignSelf: 'center' }}>2</Text>
+
+            </Modal>
+
+            <Modal
+                animationType="slide"
+                presentationStyle='pageSheet'
+                visible={modalVisible3}
+                onRequestClose={() => {
+                    setModalVisible3(!modalVisible3);
+                }}>
+                <View style={{ position: 'absolute', backgroundColor: 'grey', top: 10, alignContent: 'center', height: 5, width: '10%', alignSelf: 'center', borderRadius: 10 }}>
+                </View>
+                <Text style={{ fontSize: 80, alignSelf: 'center' }}>3</Text>
+            </Modal>
+
             <Text style={{ left: 40, fontFamily: 'OpenSans_700Bold', marginTop: '70%', fontSize: 25, color: "#3D3D3D" }}>Important Information</Text>
             <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{ paddingTop: 7, paddingLeft: 40 }} snapToInterval={370} decelerationRate={0}>
-                <Pressable onPress={() => setModalVisible(true)} style={({ pressed }) => [
+                <Pressable onPress={() => setModalVisible1(true)} style={({ pressed }) => [
                     {
                         opacity: pressed
                             ? '0.5'
@@ -39,7 +70,13 @@ export default function ImpEvents() {
                         style={{ width: 350, height: 175, borderRadius: 10 }}
                     />
                 </Pressable>
-                <Pressable onPress={() => setModalVisible(true)}>
+                <Pressable onPress={() => setModalVisible2(true)} style={({ pressed }) => [
+                    {
+                        opacity: pressed
+                            ? '0.5'
+                            : '1'
+                    },
+                ]}>
                     <Image
                         source={{
                             uri: slides[1]
@@ -47,7 +84,13 @@ export default function ImpEvents() {
                         style={{ width: 350, height: 175, borderRadius: 10, marginLeft: 20 }}
                     />
                 </Pressable>
-                <Pressable onPress={() => setModalVisible(true)}>
+                <Pressable onPress={() => setModalVisible3(true)} style={({ pressed }) => [
+                    {
+                        opacity: pressed
+                            ? '0.5'
+                            : '1'
+                    },
+                ]}>
                     <Image
                         source={{
                             uri: slides[2]
