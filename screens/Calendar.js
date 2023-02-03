1c70ae9286c80c248869e174d813fcb83549002f
Jake Steck <jakestecklownj@gmail.com>
2023-02-03 15:53:59 -0500

    Added Calendar Screen

---
+++ screens/Calendar.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Pressable } from 'react-native';
+
+export default function Calendar({ navigation }) {
+    return (
+        <View style={{ backgroundColor: 'white', flex: 1 }}>
+            <Text>Calendar</Text>
+        </View>
+    )
+}
