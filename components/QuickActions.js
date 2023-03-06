dbd2ec066078b8193adbc4c73c9c1719d483e3fd
Jake Steck <jakestecklownj@gmail.com>
2023-03-06 18:34:11 -0500

    Quick Actions

--- components/QuickActions.js
+++ components/QuickActions.js
@@ -4,41 +4,58 @@ import tw from 'tailwind-react-native-classnames';
 import { useState } from 'react';
 import Map from './Map';
 import { MaterialCommunityIcons } from 'react-native-vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { Link, useNavigation } from '@react-navigation/native';
 import { SchoolDirections } from './SchoolDirectionsModal';
+import { Linking } from 'react-native';
+import PhotoShare from '../screens/PhotoShare';
 
-export default function QuickActions({ navigation }) {
+export default function QuickActions() {
+
+    const navigation = useNavigation();
 
     const [SchoolDirections, setModalVisibleMap] = useState(false);
 
-    const navigation1 = useNavigation();
+
+
+    const handleSchoology = () => {
+        Linking.openURL('https://livingston.schoology.com/');
+    }
+
+    const handleGenesis = () => {
+        Linking.openURL('https://students.livingston.org/');
+    }
+
+    const openMaps = () => {
+        Linking.openURL('http://maps.apple.com/?q=Livingston+High+School+New+Jersey');
+    }
+
+
 
     return (
         <>
 
-        <Modal
-            animationType="slide"
-            presentationStyle='pageSheet'
-            visible={SchoolDirections}
-            onRequestClose={() => {
-                setModalVisibleMap(!SchoolDirections);
-            }}>
-            <SchoolDirections></SchoolDirections>
-        </Modal>
 
-        <Text style={{ left: 40, fontFamily: 'OpenSans_700Bold', marginTop: '10%', fontSize: 25, color: "#3D3D3D" }}>Quick Actions</Text>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{ paddingTop: 7, paddingLeft: 40 }} snapToInterval={370} decelerationRate={0}>
-                <Pressable style = {({ pressed }) => [{opacity: pressed ? 0.5: 1, borderWidth: 3, borderColor: "#3D3D3D" , borderRadius: 10 }]} onPress={() => {
-                    setModalVisibleMap(true)
-                }} >
-                    <MaterialCommunityIcons name="map-search" size={100} style = {{ color: "#3D3D3D" }}/>
-                </Pressable>
-                <Pressable style = {({ pressed }) => [{opacity: pressed ? 0.5: 1, borderWidth: 3, borderColor: "#3D3D3D" , borderRadius: 10, marginLeft: 15 }]} onPress={() => navigation1.navigate("Photos")}>
-                    <MaterialCommunityIcons name="share" size={100} style = {{ color: "#3D3D3D" }}/>
-                </Pressable>
-            </ScrollView >
-        </View>
+            <Text style={{ left: 40, fontFamily: 'OpenSans_700Bold', marginTop: '10%', fontSize: 25, color: "#3D3D3D" }}>Quick Actions</Text>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{ paddingTop: 7, paddingLeft: 40 }} snapToInterval={250} decelerationRate={0}>
+                    <Pressable style={({ pressed }) => [{ opacity: pressed ? 0.5 : 1 }]} onPress={openMaps}>
+                        <Image source={require('../assets/imgs/quick/direct.png')} style={{ width: 230, height: 175, borderRadius: 10 }}
+                        />
+                    </Pressable>
+                    <Pressable style={({ pressed }) => [{ opacity: pressed ? 0.5 : 1 }]} onPress={handleGenesis}>
+                        <Image source={require('../assets/imgs/quick/gen.png')} style={{ width: 230, height: 175, borderRadius: 10, marginLeft: 20 }}
+                        />
+                    </Pressable>
+                    <Pressable style={({ pressed }) => [{ opacity: pressed ? 0.5 : 1 }]} onPress={handleSchoology}>
+                        <Image source={require('../assets/imgs/quick/school.png')} style={{ width: 230, height: 175, borderRadius: 10, marginLeft: 20 }}
+                        />
+                    </Pressable>
+                    <Pressable style={({ pressed }) => [{ opacity: pressed ? 0.5 : 1 }]} onPress={() => navigation.navigate("PhotoShare")}>
+                        <Image source={require('../assets/imgs/quick/share.png')} style={{ width: 230, height: 175, borderRadius: 10, marginLeft: 20 }}
+                        />
+                    </Pressable>
+                </ScrollView >
+            </View>
         </>
     )
 }
--- screens/Home.js
+++ screens/Home.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
 import HomeHeader from '../components/HomeHeader';
 import ImpEvents from '../components/ImpEvents';
 import Sports from '../components/Sports';
+import QuickActions from '../components/QuickActions';
 
 export default function Home() {
 
@@ -14,6 +15,7 @@ export default function Home() {
             <ScrollView contentContainerStyle={{ bottom: 20 }}>
                 <ImpEvents />
                 <Sports />
+                <QuickActions />
             </ScrollView>
         </>
     );
